b1e3ce1138195d61ff1802c6e0a453558c31f5f7
aaronS7 <ajscirocco7+dash@gmail.com>
2021-04-05 05:29:44 +0000

    Fix gitpod config

--- gitpodInit.sh
+++ gitpodInit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py
-mkdir /workspace/poetry && POETRY_HOME=/workspace/poetry python get-poetry.py -y && rm get-poetry.py
+mkdir -p /workspace/poetry && POETRY_HOME=/workspace/poetry python get-poetry.py -y && rm get-poetry.py
+echo "export PATH=/workspace/poetry/env:$PATH" >> /home/gitpod/.profile && source /workspace/poetry/env && echo "path is $(printenv PATH)"
+poetry install
