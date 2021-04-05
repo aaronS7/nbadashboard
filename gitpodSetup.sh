39fcba8343483b1d9f9034b51aec01ee7ec05301
aaronS7 <ajscirocco7@gmail.com>
2021-04-04 21:58:09 -0700

    Update gitpodSetup.sh

--- gitpodSetup.sh
+++ gitpodSetup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 touch /workspace/aarpn.txt
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py && POETRY_HOME=/workspace/poetry python get-poetry.py -y && rm get-poetry.py
 echo "export PIP_USER=no" >> /home/gitpod/.profile && echo "export POETRY_CACHE_DIR=/workspace/.cache/pypoetry" >> /home/gitpod/.profile
 source /home/gitpod/.profile
