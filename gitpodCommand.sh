052332c896564eced60aa2ddcd0a243c7f3a158f
aaronS7 <ajscirocco7+dash@gmail.com>
2021-04-05 05:41:54 +0000

    Fix gitpod config files

--- gitpodCommand.sh
+++ gitpodCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+source /home/gitpod/.profile
 echo "export POETRY_MODULES=$(realpath $(find $(POETRY_CACHE_DIR) -path "*/bin"))" >> /home/gitpod/.profile && source /home/gitpod/.profile
 echo "export PATH=$POETRY_MODULES:$PATH" >> /home/gitpod/.profile
-source /home/gitpod/.profile
--- gitpodSetup.sh
+++ gitpodSetup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "export PIP_USER=no" >> /home/gitpod/.profile
 echo "export POETRY_CACHE_DIR=/workspace/.cache/pypoetry" >> /home/gitpod/.profile
-source /home/gitpod/.profile
